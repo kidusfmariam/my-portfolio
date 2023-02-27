e0367994f75cc2a5f247f04f924b1dbc63f811a6
kidusfmariam <99491498+kidusfmariam@users.noreply.github.com>
2023-02-26 21:25:32 -0800

    scroll to top function added

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import ProjectDetail from "./Pages/ProjectDetail"
 import Contact from "./Pages/Contact"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./Components/Navbar/Navbar"
+import ScrollToTop from "./Components/ScrollToTop/ScrollToTop"
 function App() {
 
   return (
       <>
         <Router>
+          <ScrollToTop/>
           <Navbar/>
             <Routes>
               <Route path="/" element={<Home/>}/>
