2b16e323c084026764f008f696c31c04bbd4f45c
kidusfmariam <99491498+kidusfmariam@users.noreply.github.com>
2023-02-26 19:06:55 -0800

    Contact Page Completed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav>
-      <h2>K.A</h2>
-      <h2>Contact Me</h2>
+      <Link style={{textDecoration: 'none'}} to='/'><h2>K.A</h2></Link>
+      <Link style={{textDecoration: 'none'}} to='/contact'><h2>Contact Me</h2></Link>
     </nav>
   )
 }
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import './Contact.css'
+import {AiOutlineMail, AiOutlineGithub, AiOutlineTwitter, AiOutlineLinkedin} from 'react-icons/ai'
+import {FaTelegramPlane} from 'react-icons/fa'
 const Contact = () => {
   return (
-    <div>
-      
+    <div className='contact-container'>
+        <div className='contact-wrapper'>
+            <h1>Contact Me</h1>
+            <a href="mailto:kidusfmariamayalew@gmail.com" target='_blank'>
+              <p className='email'>kidusfmariamayalew@gmail.com</p>
+            </a>
+            <div className='socials'>
+              <a href="https://t.me/wanderingsailboat" target='_blank'>
+                <FaTelegramPlane/>
+              </a>
+              <a href="https://github.com/kidusfmariam" target='_blank'>
+                <AiOutlineGithub/>
+              </a>
+              <a href="#" target='_blank'>
+                <AiOutlineTwitter/>
+              </a>
+              <a href="#" target='_blank'>
+                <AiOutlineLinkedin/>
+              </a>
+            </div>
+        </div>
     </div>
   )
 }
